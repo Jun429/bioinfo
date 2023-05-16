17d687cbb2137d4f7bc049d82975dc289ee04559
jjxu <jjxu@aielab.net>
2023-05-16 14:49:34 +0800

    local_uwsgi

--- bioinfo/urls.py
+++ bioinfo/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from laboratory import views
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('bioinfolab/',include('laboratory.urls')),
     path('manager/', include('manager.urls')),
